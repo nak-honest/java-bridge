5d2ae520e9bebd4af3addb03d0b62ec776d7c088
nhlee98 <e5slnh9876@gmail.com>
2023-12-14 16:17:44 +0900

    refactor: BridgeLength의 기능을 Bridge로 통합

---
+++ src/main/java/bridge/domain/Bridge.java
@@ -0,0 +1,35 @@
+package bridge.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class Bridge {
+    private static final int MIN_LENGTH = 3;
+    public static final int MAX_LENGTH = 20;
+
+    private final List<Section> sections;
+
+    private Bridge(List<Section> sections) {
+        this.sections = new ArrayList<>(sections);
+        validateLength();
+    }
+
+    private void validateLength() {
+        if (sections.size() < MIN_LENGTH || sections.size() > MAX_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 다리 길이는 %d부터 %d 사이의 숫자여야 합니다.", MIN_LENGTH, MAX_LENGTH));
+        }
+    }
+
+    public static Bridge of(BridgeMaker bridgeMaker, int length) {
+        return new Bridge(bridgeMaker.makeBridge(length).stream()
+                .map(Section::of)
+                .collect(Collectors.toList()));
+    }
+
+    public List<Section> getCollection() {
+        return Collections.unmodifiableList(sections);
+    }
+}
--- src/test/java/bridge/domain/BridgeTest.java
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -1,5 +1,7 @@
 package bridge.domain;
 
+import bridge.BridgeNumberGenerator;
+import bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,11 +13,16 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class BridgeLengthTest {
+public class BridgeTest {
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 1, 2, 21, 22})
+    @ValueSource(ints = {0, 1, 2, 21, 22})
     void 다리의_길이가_3부터_20_사이의_숫자가_아닌_경우_예외를_발생시킨다(int length) {
-        assertThatThrownBy(() -> new BridgeLength(length))
+        // given
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+
+        // when & then
+        assertThatThrownBy(() -> Bridge.of(bridgeMaker, length))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.");
     }
@@ -23,7 +30,7 @@ public class BridgeLengthTest {
     @ParameterizedTest
     @MethodSource("provideValidLengths")
     void 다리의_길이가_3부터_20_사이의_숫자인_경우_예외를_발생시키지_않는다(int length) {
-        assertThatCode(() -> new BridgeLength(length))
+        assertThatCode(() -> Bridge.of(new BridgeMaker(new BridgeRandomNumberGenerator()), length))
                 .doesNotThrowAnyException();
     }
 

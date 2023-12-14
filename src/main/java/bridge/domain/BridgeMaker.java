4a1dbcaba5d15140d54c425cfeca32f92dc91947
nhlee98 <e5slnh9876@gmail.com>
2023-12-14 15:54:27 +0900

    feat: 입력 받은 길이의 다리를 생성한다

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -2,12 +2,20 @@ package bridge.domain;
 
 import bridge.BridgeNumberGenerator;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
  */
 public class BridgeMaker {
+    private static final Map<Integer, String> SECTION = Map.of(
+            0, "D",
+            1, "U"
+    );
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
@@ -20,6 +28,9 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        return Stream.generate(bridgeNumberGenerator::generate)
+                .limit(size)
+                .map(SECTION::get)
+                .collect(Collectors.toList());
     }
 }

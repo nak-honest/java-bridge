1dac5f8642fe5d9bb93145c284064c4edbe90ac8
nhlee98 <e5slnh9876@gmail.com>
2023-12-14 18:40:14 +0900

    feat: 재시도 라면 이전까지 유저가 이동한 칸을 초기화한다

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -1,6 +1,7 @@
 package bridge.domain;
 
 import java.util.Collections;
+import java.util.List;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
@@ -32,6 +33,11 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        roundResult = new RoundResult(Collections.emptyList());
+    }
+
+    public List<SectionResult> getResults() {
+        return roundResult.getResults();
     }
 
     public GameState getCurrentState() {
